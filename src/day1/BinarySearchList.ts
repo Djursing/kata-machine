8126d8a3fd024f955962e88e666ce6f55ae0cc73
Oliver Djursing <Oliverdjursing@gmail.com>
2023-06-24 18:02:07 +0200

    feat: implement BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,1 +1,21 @@
-export default function bs_list(haystack: number[], needle: number): boolean {}
+export default function bs_list(haystack: number[], needle: number): boolean {
+        var lo = 0;
+        var hi = haystack.length;
+
+        while (lo < hi) {
+                const mid = Math.floor(lo + (hi - lo) / 2);
+                const val = haystack[mid];
+
+                if (val === needle) {
+                        return true;
+                }
+
+                if (val > needle) {
+                        hi = mid;
+                } else {
+                        lo = mid + 1;
+                }
+        }
+
+        return false;
+}
