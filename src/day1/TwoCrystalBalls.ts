e3b6134f648e77c7eceb75fbc319458b6a523f00
Oliver Djursing <Oliverdjursing@gmail.com>
2023-06-30 23:32:24 +0200

    Solve TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,1 +1,15 @@
-export default function two_crystal_balls(breaks: boolean[]): number {}
+export default function two_crystal_balls(breaks: boolean[]): number {
+        const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+
+        for (let i = 0; i < breaks.length; i += jumpAmount) {
+                if (breaks[i]) {
+                        for (let j = i - jumpAmount; j < i; j++) {
+                                if (breaks[j]) {
+                                        return j;
+                                }
+                        }
+                }
+        }
+
+        return -1;
+}
